f62eb0c337368a292e4ee7be4c8633ed669f788a
R.J <rjschodowski@hotmail.com>
2021-08-02 15:18:36 -0400

    task-item component updated

--- task-tracker/src/app/components/task-item/task-item.component.ts
+++ task-tracker/src/app/components/task-item/task-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-task-item',
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./task-item.component.css']
 })
 export class TaskItemComponent implements OnInit {
+@Input() task: Task
 
   constructor() { }
 
